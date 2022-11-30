f5b130e67a319f15c389127426ea18fff8d27bb8
vaibhav <vaibhavsutar264@gmail.com>
2022-11-30 15:39:53 +0530

    added full upated code

--- src/types/authType.ts
+++ src/types/authType.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 0eddcf581e4fbecfb12b1c293db8fb859a84ab49
 export type AuthState = {
   user: null | AuthUser
   profile: Profile | undefined
@@ -10,10 +6,7 @@ export type AuthState = {
   isSuccess: boolean
   isAuthenticated: boolean
   message: string | undefined
-<<<<<<< HEAD
   emailSent: string | undefined
-=======
->>>>>>> 0eddcf581e4fbecfb12b1c293db8fb859a84ab49
 }
 
 export type AuthUser = {
