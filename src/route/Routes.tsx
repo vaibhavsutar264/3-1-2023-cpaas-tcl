aa17c1a81b8a8756012b3d0fb7adab4844bb114f
vaibhav <vaibhavsutar264@gmail.com>
2022-12-15 11:01:24 +0530

    private routes fixed

--- src/route/Routes.tsx
+++ src/route/Routes.tsx
@@ -6,57 +6,68 @@ import { useRoutes } from 'react-router-dom'
 
 // eslint-disable-next-line react/display-name
 const Loadable = (Component: any) => (props: any) => {
-    return (
-        <Suspense fallback={<Loading />}>
-            <Component {...props} />
-        </Suspense>
-    )
+  return (
+    <Suspense fallback={<Loading />}>
+      <Component {...props} />
+    </Suspense>
+  )
 }
 
 function Routes({ toggleTheme }: { toggleTheme: any }) {
-    return useRoutes([
-        // { path: appRoutes.ROOT, element: { <HomeScreen /> } },
+  return useRoutes([
+    // { path: appRoutes.ROOT, element: { <HomeScreen /> } },
+    {
+      path: '',
+      children: [
+        { path: appRoutes.LOGIN, element: <Login /> },
+        { path: appRoutes.ROOT, element: <HomeScreen /> },
+        { path: appRoutes.RESET_PASSWORD, element: <ResetPassword /> },
+        { path: appRoutes.FORGOT_PASSWORD, element: <ForgotPassword /> },
+      ],
+    },
+    {
+      path: '',
+      element: <PrivateRoutes />,
+      children: [
+        // { path: appRoutes.INVOICE, element: <InvoiceBill /> },
         {
-            path: '',
-            children: [
-                { path: appRoutes.LOGIN, element: <Login /> },
-                { path: appRoutes.ROOT, element: <HomeScreen /> },
-                { path: appRoutes.RESET_PASSWORD, element: <ResetPassword /> },
-                { path: appRoutes.FORGOT_PASSWORD, element: <ForgotPassword /> },
-                // { path: appRoutes.SET_PASSWORD, element: <SetPassword /> },
-                // { path: appRoutes.BILLING, element: <Billing toggleTheme={toggleTheme} /> },
-            ],
+          path: appRoutes.BILLING,
+          element: <Billing toggleTheme={toggleTheme} />,
         },
-        {
-            path: '',
-            element: <PrivateRoutes />,
-            children: [
-                { path: appRoutes.INVOICE, element: <InvoiceBill /> },
-                { path: appRoutes.RAISE_TICKET, element: <RaiseTicket /> },
-                { path: appRoutes.BILLING, element: <Billing toggleTheme={toggleTheme} /> },
-                { path: appRoutes.BILLING_DETAILS, element: <BillingDetails /> },
-                {
-                  path: appRoutes.SET_PASSWORD,
-                  element: <SetPassword />,
-                },
-            ],
-        },
-        {
-            path: appRoutes.NOT_FOUND,
-            element: <Notfound />,
-        },
-    ])
+        // { path: appRoutes.BILLING_DETAILS, element: <BillingDetails /> },
+        { path: appRoutes.SET_PASSWORD, element: <SetPassword /> },
+      ],
+    },
+    {
+      path: appRoutes.NOT_FOUND,
+      element: <Notfound />,
+    },
+  ])
 }
 
-const Login = Loadable(lazy(() => import('../components/login/login-screen/Login')));
-const ResetPassword = Loadable(lazy(() => import('../components/login/reset-password/ResetPassword')));
-const ForgotPassword = Loadable(lazy(() => import('../components/login/forgot-password/ForgotPassword')));
-const SetPassword = Loadable(lazy(() => import('../components/login/set-password/SetPassword')));
-const Notfound = Loadable(lazy(() => import('../components/notfound/Notfound')));
-const HomeScreen = Loadable(lazy(() => import('../components/home/HomeScreen')));
-const Billing = Loadable(lazy(() => import('../components/billing/Billing')));
-const InvoiceBill = Loadable(lazy(() => import('../components/billing/InvoiceBill')));
-const RaiseTicket = Loadable(lazy(() => import('../components/billing/RaiseTicket')));
-const BillingDetails = Loadable(lazy(() => import('../components/billing/BillingDetails')));
+const Login = Loadable(
+  lazy(() => import('../components/login/login-screen/Login'))
+)
+const ResetPassword = Loadable(
+  lazy(() => import('../components/login/reset-password/ResetPassword'))
+)
+const ForgotPassword = Loadable(
+  lazy(() => import('../components/login/forgot-password/ForgotPassword'))
+)
+const SetPassword = Loadable(
+  lazy(() => import('../components/login/set-password/SetPassword'))
+)
+const Notfound = Loadable(lazy(() => import('../components/notfound/Notfound')))
+const HomeScreen = Loadable(lazy(() => import('../components/home/HomeScreen')))
+const Billing = Loadable(lazy(() => import('../components/billing/Billing')))
+const InvoiceBill = Loadable(
+  lazy(() => import('../components/billing/InvoiceBill'))
+)
+const RaiseTicket = Loadable(
+  lazy(() => import('../components/billing/RaiseTicket'))
+)
+const BillingDetails = Loadable(
+  lazy(() => import('../components/billing/BillingDetails'))
+)
 
 export default Routes
