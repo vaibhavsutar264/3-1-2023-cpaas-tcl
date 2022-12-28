6dee2720c1151415d10f0769dfc2200a03e18428
lalith-at-992512424612 <lalith889@gmail.com>
2022-12-28 11:15:21 +0530

    aded

--- src/components/account-details/account-components/AccountAvatar.tsx
+++ src/components/account-details/account-components/AccountAvatar.tsx
@@ -30,7 +30,7 @@ const AccountAvatar = () => {
       setTimezone(user.attributes.timezone)
       setCommunication(user.attributes.preferredCommunicationMode)
     }
-  }, [dispatch,user])
+  }, [])
 
 
   const editUserDetails = async (e: SyntheticEvent) => {
